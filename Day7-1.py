091bb3903837898c2a88d20f8f26065be5d3d225
Chris Perkins <Cperkins91@gmail.com>
2022-12-15 00:57:04 -0600

    Day 7: Fixed the findSize method. Children were being appended as str instead of objects. Fixed by changing line 16 from self.name to self.

--- Day7-1.py
+++ Day7-1.py
@@ -1,55 +1,69 @@
+from __future__ import annotations
+from typing import Optional, List, Generator, Callable
+from operator import ge, le
+from functools import cached_property
+
+
 class elfFolder:
-    def __init__(self, name, parent):
+    def __init__(self, name: str, parent: Optional[elfFolder], size: Optional[int],
+                 children: Optional[List[elfFolder]]):
         self.name = name
-        self.children = []
-        # self.children = children
-        # self.files = files
+        self.size = size
         self.parent = parent
+        self.children = children
+        self.isFile = True
         if parent is not None:
-            parent.addChild(name)
+            parent.addChild(self)
 
-    def newFolder(self, name, parent):
-        self.name = name
-        self.parent = parent
-
-    def addChild(self, children):
-        self.children += children
-
-    def addFile(self, elfFile):
-        self.children.append(elfFile)
-
-    def size(self):
-        sum = 0
+    def __str__(self) -> str:
+        indent, parent = "", self.parent
+        while parent:
+            indent += "  "
+            parent = parent.parent
+        stub = " ".join([indent, "-", str((self.name, self.size))])
+        if self.isFile:
+            return stub
         for child in self.children:
-            if child is elfFile:
-                sum += child.size
-            elif child is elfFolder:
-                sum += child.size()
-        return sum
-        #print(f"The directory {self.name} has a size of {sum}")
+            stub += "\n" + str(child)
+        return stub
+
+    @property
+    def findSize(self):
+        if self.size is None:
+            for child in self.children:
+                if self.size is None:
+                    self.size = child.findSize
+                else:
+                   self.size += child.findSize
+            # self.size = sum(child.size for child in self.children)
+#       else:
+#            self.size += child.findSize
+        return self.size
+
+    def addChild(self, children: elfFolder):
+        self.children.append(children)
+        self.isFile = False
 
 
-class elfFile(elfFolder):
-    def __init__(self, name, size, parent):
-        self.name = name
-        self.size = size
-        self.parent = parent
-        if parent is not None:
-            parent.addFile(name)
-
-    def newFile(self, size, name):
-        self.name = name
-        self.size = size
+'''
+    @property
+    def size(self) -> int:
+        if self._size is None:
+            self._size = sum([child.size for child in self.children])
+        return self._size
+'''
 
 
 def main():
-    root = elfFolder("/", None)
-    a = elfFolder("a", parent=root)
-    file1 = elfFile("file1", 949, a)
-    file2 = elfFile("file2", 420, a)
-    file3 = elfFile("file3", 666, root)
-    print(root.size())
-    print(type(a))
+    root = elfFolder("/", None, None, [])
+    a = elfFolder("a", root, None, [])
+    b = elfFolder("b", a, None, [])
+    file1 = elfFolder("file1", a, 444, [])
+    file2 = elfFolder("file2", a, 69, [])
+    file3 = elfFolder("file3", root,333,[])
+    print(root.findSize)
+    print(file1)
+
 
 
 '''
