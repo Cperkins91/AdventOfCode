a80ebfb6eb4f8615733fb024a5bf348bf0f24510
Chris Perkins <Cperkins91@gmail.com>
2022-12-15 02:11:07 -0600

    Day 7: Was able to ingest the data and build the directory tree. Need to sum the directories and output the big ones.

--- Day7-1.py
+++ Day7-1.py
@@ -12,6 +12,7 @@ class elfFolder:
         self.parent = parent
         self.children = children
         self.isFile = True
+        ''' Add this if you want to add children in the init'''
         if parent is not None:
             parent.addChild(self)
 
@@ -34,10 +35,7 @@ class elfFolder:
                 if self.size is None:
                     self.size = child.findSize
                 else:
-                   self.size += child.findSize
-            # self.size = sum(child.size for child in self.children)
-#       else:
-#            self.size += child.findSize
+                    self.size += child.findSize
         return self.size
 
     def addChild(self, children: elfFolder):
@@ -45,34 +43,40 @@ class elfFolder:
         self.isFile = False
 
 
-'''
-    @property
-    def size(self) -> int:
-        if self._size is None:
-            self._size = sum([child.size for child in self.children])
-        return self._size
-'''
-
-
 def main():
+    lines = open('cmdlines.txt', 'rt').read()
+    lines = lines.split('\n')
+    root = curr_dir = elfFolder(lines.pop(0).split()[-1], None, None, [])
+    for line in lines:
+        line = line.split()
+        if line[0] == '$':  # Line is a command
+            if line[1] == 'cd':
+                if line[2] == '..':
+                    curr_dir = curr_dir.parent  # Change to new directory
+                else:
+                    child_dir = elfFolder(line[2], curr_dir, None, [])
+                    curr_dir.addChild(child_dir)
+                    curr_dir = child_dir
+            if line[1] == 'ls':
+                pass
+        # if line[0] == 'dir': # line is folder
+        #    elfFolder(line[1], curr_dir, None, None)
+        if line[0].isdigit():  # line is a file:
+            child_dir = elfFolder(line[1], curr_dir, int(line[0]), None)
+            #curr_dir.addChild(child_dir) #Add this if you dont want to use the
+            #conditional inside the init
+        print(line[0])
+
+
+'''
     root = elfFolder("/", None, None, [])
     a = elfFolder("a", root, None, [])
     b = elfFolder("b", a, None, [])
     file1 = elfFolder("file1", a, 444, [])
     file2 = elfFolder("file2", a, 69, [])
-    file3 = elfFolder("file3", root,333,[])
+    file3 = elfFolder("file3", root, 333, [])
     print(root.findSize)
     print(file1)
-
-
-
-'''
- lines = open('cmdlines.txt', 'rt').read()
- lines = lines.split('\n')
- for line in lines:
-     current = line.split()
-     if current[0] == '$':
-         if current[1] == cd:
 '''
 
 if __name__ == '__main__':
